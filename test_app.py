fcea2b132523d8bb29ec835f165faef7fdce007d
angelica123-nf <sitiberkah.ha@gmail.com>
2025-06-11 20:57:15 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,7 +3,5 @@ from app import app
 def test_hello():
     client = app.test_client()
     response = client.get('/')
-    assert respons.status_code == 200
+    assert response.status_code == 200
     assert b"Hello from Jenkins Multibranch Pipeline" in response.data
-
-
